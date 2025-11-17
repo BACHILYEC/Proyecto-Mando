ff90c1e8cfd141250485d76733a406d1937006c6
KarliBoo <karli_cruz29@hotmail.com>
2025-11-17 12:04:18 -0500

    Poniendole mas onito

--- src/pkTrivia/pkArchivo/NumeroReader.java
+++ src/pkTrivia/pkArchivo/NumeroReader.java
@@ -8,49 +8,49 @@ import java.util.Scanner;
 
 public class NumeroReader {
 
-    Scanner sc = new Scanner(System.in);
+   Scanner sc = new Scanner(System.in);
 
-    public String GetPathFile() {
-        String PathFile = "";
-        System.out.println("Elige el nivel:");
-        System.out.println("1. Nivel 1");
-        System.out.println("2. Nivel 2");
+   public String GetPathFile() {
+      String PathFile = "";
+      System.out.println("Elige el nivel:");
+      System.out.println("1. Nivel 1");
+      System.out.println("2. Nivel 2");
 
-        int opcion = sc.nextInt();
-        sc.nextLine(); 
+      int opcion = sc.nextInt();
+      sc.nextLine(); 
 
-        switch (opcion) {
+      switch (opcion) {
             case 1:
-                PathFile = "File/preguntasNivel1.csv";
-                break;
+               PathFile = "File/preguntasNivel1.csv";
+               break;
 
             case 2:
-                PathFile = "File/preguntasNivel2.csv";
-                break;
+               PathFile = "File/preguntasNivel2.csv";
+               break;
 
             default:
-                System.out.println("Opción inválida");
-                break;
-        }
-        return PathFile;
-    }
+               System.out.println("Opción inválida");
+               break;
+      }
+      return PathFile;
+   }
 
-    public ArrayList<String> readFile(String pathFile) {
+   public ArrayList<String> readFile(String pathFile) {
 
-        ArrayList<String> lineas = new ArrayList<>();
+      ArrayList<String> lineas = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
+      try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
             String texto;
 
             while ((texto = br.readLine()) != null) {
-                lineas.add(texto);
+               lineas.add(texto);
             }
 
-        } catch (IOException e) {
+      } catch (IOException e) {
             e.printStackTrace();
-        }
+      }
 
-        return lineas;
+      return lineas;
     }
 }
 
